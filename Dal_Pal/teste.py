00401277ab5278f93231ee831dd803623dce8121
Welton Vaz de Souza <weltonvaz@gmail.com>
2017-02-22 10:04:52 -0300

    Update
    Acho que entendi o que vc precisava

--- Dal_Pal/teste.py
+++ Dal_Pal/teste.py
@@ -1,16 +1,13 @@
-import os
-os.system('clear')
-
 import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import interpolate
 
-
 x, y = np.loadtxt("Precos.txt", delimiter=",", unpack=True)
 f = interpolate.interp1d(x, y)
 
-ynew = f(xnew)
+xnew = np.arange(1, 10, 0.1)
+ynew = f(xnew)   # use interpolation function returned by `interp1d`
 
 plt.plot(x, y, 'o', xnew, ynew, '-')
 plt.show()
